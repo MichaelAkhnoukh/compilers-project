5deae00dc27f6f72827ca20fd0e1dea883861263
MichaelAkhnoukh <eng.michaelakhnoukh@gmail.com>
2018-11-02 20:45:08 +0200

    add line parser

--- main.cpp
+++ main.cpp
@@ -2,120 +2,87 @@
 #include<stdlib.h>
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <cstring>
+#include <cstdarg>
+#include <regex>
+
 using namespace std;
-typedef enum{START,INCOMMENT,INID,INASSIGN,INNUM,DONE}States;
-string ReservedKeywords [8]={"if","then","else","end","repeat","until","read","write"};
-string SpecialSymbols [12]={"+","-","*","/","=","<","(",")",";",":=","{","}"};
+typedef enum {
+    START, INCOMMENT, INID, INASSIGN, INNUM, DONE
+} States;
+string ReservedKeywords[8] = {"if", "then", "else", "end", "repeat", "until", "read", "write"};
+string SpecialSymbols[12] = {"+", "-", "*", "/", "=", "<", "(", ")", ";", ":=", "{", "}"};
+
+int Scanner(char c) {
+    static int state = START;
+    bool save;
+    switch (state) {
+        case START:
+            break;
+        case INCOMMENT:
+            break;
+        case INID:
+            break;
+        case INASSIGN:
+            break;
+        case INNUM:
+            break;
+        case DONE:
+            break;
+    }
+
+
+}
+
 typedef struct {
+    unsigned char state;
+    unsigned char tokentype;
+    unsigned char tokenvalue;
 
-};
-int Scanner(char c){
-	static int state=START;
-	bool save;
-	switch (state){
-	case START:
-		if (isdigit(c))
-			state = INNUM;
-		else if (isalpha(c))
-			state = INID;
-		else if (c == ':')
-			state = INASSIGN;
-		else if ((c == ' ') || (c == '\t') || (c == '\n'))
-			save = false;
-		else if (c == '{')
-		{ save = false;
-			state = INCOMMENT;
-		}
-		else
-		{ state = DONE;
-			switch (c)
-			{ case EOF:
-					save = false;
-					currentToken = ENDFILE;
-					break;
-				case '=':
-					currentToken = EQ;
-					break;
-				case '<':
-					currentToken = LT;
-					break;
-				case '+':
-					currentToken = PLUS;
-					break;
-				case '-':
-					currentToken = MINUS;
-					break;
-				case '*':
-					currentToken = TIMES;
-					break;
-				case '/':
-					currentToken = OVER;
-					break;
-				case '(':
-					currentToken = LPAREN;
-					break;
-				case ')':
-					currentToken = RPAREN;
-					break;
-				case ';':
-					currentToken = SEMI;
-					break;
-				default:
-					currentToken = ERROR;
-					break;
-			}
-		}
-		break;
-	case INCOMMENT:
-		break;
-	case INID:
-		break;
-	case INASSIGN:
-		break;
-	case DONE:
-		break;
-	}
+} jkj;
 
+void get(const char *input) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; SpecialSymbols[i][j] != 0; j++) {
+            for (int k = 0; input[k] != 0; k++) {
+                if (input[k] == SpecialSymbols[i][j]) {
 
+                }
+            }
+            printf("%c", SpecialSymbols[i][j]);
+            fflush(stdout);
+        }
+    }
 }
-typedef struct{
-	unsigned char state;
-	unsigned char tokentype;
-	unsigned char tokenvalue;
 
-}jkj;
-void get(const char *input){
-	for(int i=0;i<10;i++){
-		for(int j=0;SpecialSymbols[i][j]!=0;j++){
-			for(int k=0;input[k]!=0;k++){
-				if(input[k]==SpecialSymbols[i][j]){
-
-				}
-			}
-			printf("%c",SpecialSymbols[i][j]);
-			fflush(stdout);
-		}
-	}
+string *splitLine(string &line) {
+//    line = regex_replace(line,regex("\\{.*\\}")," ");
+    string *words = new string[100];
+    char *buffer = new char[line.length() + 1];
+    strcpy(buffer, line.c_str());
+    char *wordContext;
+    const char *wordSeparators = " +-*/=<();:=";
+    char *word = strtok_r(buffer, wordSeparators, &wordContext);
+    int i = 0;
+    while (word) {
+        words[i] = word;
+        word = strtok_r(NULL, wordSeparators, &wordContext);
+        i++;
+    }
+    delete[] buffer;
+    return words;
 }
+
 //void getInput()
-int main(int argc, char *argv[]){
-//	for(int i=0;i<10;i++){
-//		for(int j=0;SpecialSymbols[i][j]!=0;j++){
-//			printf("%c",SpecialSymbols[i][j]);
-//			fflush(stdout);
-//		}
-//		printf("\n");
-//	}
+int main(int argc, char *argv[]) {
     ifstream inFile("./TinySample.txt");
-	string str;
-	while (getline(inFile,str)){
-		cout << str << endl;
-		if(str==""){
-			cout << "new line" << endl;
-		}
-
-	}
-
+    string str = "if < x   then     {  don't compute if x <=0}";
+//    while (getline(inFile, str)) {
+//
+//    }
+    string * words = splitLine(str);
+    for (int i = 0; i < 10; ++i) {
+        cout << words[i] << endl;
+    }
 }
 
