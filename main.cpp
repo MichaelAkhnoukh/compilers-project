299cac3524464c24cfee34635fc80c9f74fd03f1
MichaelAkhnoukh <eng.michaelakhnoukh@gmail.com>
2018-11-02 17:58:51 +0200

    inital parsing commit

--- main.cpp
+++ main.cpp
@@ -4,13 +4,68 @@
 #include <fstream>
 #include <string>
 using namespace std;
-typedef enum{START,INCOMMENT,INID,INASSIGN,DONE}States;
-unsigned char ReservedKeywords [][8]={"if","then","else","end","repeat","until","read","write"};
-unsigned char SpecialSymbols [][10]={"+","-","*","/","=","<","(",")",";",":="};
-int Scanner(){
-	static int current_state=START;
-	switch (current_state){
+typedef enum{START,INCOMMENT,INID,INASSIGN,INNUM,DONE}States;
+string ReservedKeywords [8]={"if","then","else","end","repeat","until","read","write"};
+string SpecialSymbols [12]={"+","-","*","/","=","<","(",")",";",":=","{","}"};
+typedef struct {
+
+};
+int Scanner(char c){
+	static int state=START;
+	bool save;
+	switch (state){
 	case START:
+		if (isdigit(c))
+			state = INNUM;
+		else if (isalpha(c))
+			state = INID;
+		else if (c == ':')
+			state = INASSIGN;
+		else if ((c == ' ') || (c == '\t') || (c == '\n'))
+			save = false;
+		else if (c == '{')
+		{ save = false;
+			state = INCOMMENT;
+		}
+		else
+		{ state = DONE;
+			switch (c)
+			{ case EOF:
+					save = false;
+					currentToken = ENDFILE;
+					break;
+				case '=':
+					currentToken = EQ;
+					break;
+				case '<':
+					currentToken = LT;
+					break;
+				case '+':
+					currentToken = PLUS;
+					break;
+				case '-':
+					currentToken = MINUS;
+					break;
+				case '*':
+					currentToken = TIMES;
+					break;
+				case '/':
+					currentToken = OVER;
+					break;
+				case '(':
+					currentToken = LPAREN;
+					break;
+				case ')':
+					currentToken = RPAREN;
+					break;
+				case ';':
+					currentToken = SEMI;
+					break;
+				default:
+					currentToken = ERROR;
+					break;
+			}
+		}
 		break;
 	case INCOMMENT:
 		break;
@@ -45,17 +100,21 @@ void get(const char *input){
 }
 //void getInput()
 int main(int argc, char *argv[]){
-	for(int i=0;i<10;i++){
-		for(int j=0;SpecialSymbols[i][j]!=0;j++){
-			printf("%c",SpecialSymbols[i][j]);
-			fflush(stdout);
-		}
-		printf("\n");
-	}
-    ifstream inFile("./test");
+//	for(int i=0;i<10;i++){
+//		for(int j=0;SpecialSymbols[i][j]!=0;j++){
+//			printf("%c",SpecialSymbols[i][j]);
+//			fflush(stdout);
+//		}
+//		printf("\n");
+//	}
+    ifstream inFile("./TinySample.txt");
 	string str;
 	while (getline(inFile,str)){
 		cout << str << endl;
+		if(str==""){
+			cout << "new line" << endl;
+		}
+
 	}
 
 }
