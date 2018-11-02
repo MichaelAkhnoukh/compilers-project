267120717d4d843bcd6a41864a693cb188d236fb
Abdelrahman Ahmed <abdelrhman81995@gmail.com>
2018-11-02 23:25:21 +0200

    All suported but for comments issue and keywords

--- main.cpp
+++ main.cpp
@@ -10,22 +10,23 @@ using namespace std;
 
 typedef enum{START,INCOMMENT,INID,INOPERATION,INNUM,DONE}States;
 string ReservedKeywords [8]={"if","then","else","end","repeat","until","read","write"};
-char SpecialSymbols[10]={'+','-','*','/','=','<','(',')',';',':'};
-string SpecialSymbolsTokens[10] = { "Addition","Subtraction","Multiply","Division","LessThan","OpenPrackrt","ClosePracket","EOL","Assignment" };
+char SpecialSymbols[10]={'+','-','*','/','<','(',')',';',':','='};
+string SpecialSymbolsTokens[10] = { "Addition","Subtraction","Multiply","Division","LessThan","OpenPrackrt","ClosePracket","EOL","Assignment","Comparison" };
 
 
-int Scanner(string line){
+void Scanner(string line){
 	static int state=START;
 	int index = 0;
 	while(line[index]!=NULL){
 		switch (state) {
 		case START:
 			//As long As we have white spaces do nothing
-			while (line[index] != NULL && ((line[index] == ' ') || (line[index] == '\t')))
+			while (line[index] != NULL && ( line[index] == ' ' || line[index] == '\t'))
 				index++;
 			//indicates comment beginning
-			if (line[index] == '{')
-				state = INCOMMENT;
+			if (line[index] == '{') {
+			state = INCOMMENT;
+			}
 			//indicates that we have either reserved keyword or identifier
 			else if (isalpha(line[index]))
 				state = INID;
@@ -38,34 +39,38 @@ int Scanner(string line){
 			break;
 		//Ready and Finalized
 		case INCOMMENT:
-			while (line[index] != NULL&&line[index] != '}')
+			while (line[index]!=NULL&&line[index] != '}')
 				index++;
 			if (line[index] == '}')
+				//line = regex_replace(line, regex("\\{.*\\}"), " ");
+				index++;
 				state = START;
 			break;
 		case INID:
 			//Call Michael's Function
+			while (isalpha(line[index]))
+				index++;
+			state = START;
 			break;
 		//Ready To print
 		case INOPERATION:
-			int i;
-			bool isAssignment = false;
-			for (i = 0; i < 10; i++) {
-				//the assignment operator has : & = (2 char in the string)
-				if (line[index] == SpecialSymbols[i]) {
+		{int i;
+		bool isAssignment = false;
+		for (i = 0; i < 10; i++) {
+			//the assignment operator has : & = (2 char in the string)
+			if (line[index] == SpecialSymbols[i]) {
+				index++;
+				if (SpecialSymbols[i] == ':') {
 					index++;
-					if (SpecialSymbols[i] == ':') {
-						index ++;
-						isAssignment = true;
-					}
-					break;
+					isAssignment = true;
 				}
-				
+				break;
 			}
-			isAssignment ? cout << SpecialSymbols[i] << '=' : cout << SpecialSymbols[i];
-			cout << " " << SpecialSymbolsTokens[i]<<endl;
-			state = START;
-			break;
+
+		}
+		(isAssignment) ? cout << SpecialSymbols[i] << "=,Assignment\n" : cout << SpecialSymbols[i] << "," << SpecialSymbolsTokens[i] << endl;
+		state = START;
+		break; }
 		//Ready to print
 		case INNUM:
 			while (line[index]!=NULL&&isdigit(line[index]))
@@ -86,14 +91,14 @@ typedef struct{
 
 }jkj;
 
-
+/*
 string *splitLine(string &line) {
 //    line = regex_replace(line,regex("\\{.*\\}")," ");
     string *words = new string[100];
     char *buffer = new char[line.length() + 1];
     strcpy(buffer, line.c_str());
     char *wordContext;
-    const char *wordSeparators = " +-*/=<();:=";
+    const char *wordSeparators = " +-=<();:=";
     char *word = strtok_r(buffer, wordSeparators, &wordContext);
     int i = 0;
     while (word) {
@@ -103,7 +108,7 @@ string *splitLine(string &line) {
     }
     delete[] buffer;
     return words;
-}
+}*/
 
 
 //void getInput()
@@ -115,12 +120,12 @@ int main(int argc, char *argv[]){
 //		}
 //		printf("\n");
 //	}
-    ifstream inFile("./TinySample.txt");
+   ifstream inFile("./TinySample.txt");
 	string str;
 	while (getline(inFile,str)){
 		Scanner(str);
 		}
-
+	
 
 
 }
